2ec1c9f6d123babffffc3721b7ce13445777aff7
hshimokawabe <ec2-user@ip-172-31-12-227.ec2.internal>
2021-09-28 09:23:42 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Refile.secret_key = 'b8c26d3b234ce17bb324acd91d80eb6756ef8c34d7c7fe5b066e38081d4cb0ba0ac1a227f94104d2049dd013bf2a8fb4f9bc0d518a9d5a40bd8b0fade4d66934'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
